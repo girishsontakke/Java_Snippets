d2d6d4533fb91dcfc9e9d85665cb45e12d8d3551
girishsontakke <girishsontakke7@gmail.com>
2020-10-25 15:45:34 +0530

    iterface implimentation

---
+++ src/oop/InterFacesDemo.java
@@ -0,0 +1,34 @@
+package oop;
+
+interface Writer{
+    void write();
+}
+
+class Pen implements Writer{
+    public void write(){
+        System.out.println("I am Pen");
+    }
+
+}
+class Pencil implements Writer{
+    public void write(){
+        System.out.println("I am Pencil");
+    }
+
+}
+class Kit{
+    public void doSomething(Writer w){
+        w.write();
+    }
+
+}
+public class InterFacesDemo {
+
+    public static void main(String[] args) {
+        Writer writer1 = new Pen();
+        Writer writer2 = new Pencil();
+        Kit kit = new Kit();
+        kit.doSomething(writer1);
+        kit.doSomething(writer2);
+    }
+}
