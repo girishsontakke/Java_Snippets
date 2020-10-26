c11316b906c3a4f2e7316f8041947a1689a9c7d4
girishsontakke <girishsontakke7@gmail.com>
2020-10-26 16:22:22 +0530

    implementation of default method in interface

--- src/general/FunctionalInterfaceDemo.java
+++ src/general/FunctionalInterfaceDemo.java
@@ -10,9 +10,26 @@ interface BaseInt{
     void show();
 }
 
+interface Demo{
+    void abc();
+    default void show(){
+        System.out.println("in show method");
+    }
+}
+
+class DemoImp implements Demo{
+    public void abc(){
+        System.out.println("in abc");
+    }
+}
+
 public class FunctionalInterfaceDemo {
     public static void main(String[] args){
         BaseInt obj = ()-> System.out.println("functional interface");
         obj.show();
+
+        DemoImp obj1 = new DemoImp();
+        obj1.show();
+        obj1.abc();
     }
 }
