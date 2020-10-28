bd590025696ec7045f6a85685eca8dae68e6d069
girishsontakke <girishsontakke7@gmail.com>
2020-10-28 21:19:24 +0530

    new feature in java 8 for oneline try block

---
+++ src/com/exceptionHandling/TryWithResource.java
@@ -0,0 +1,14 @@
+package com.exceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TryWithResource {
+    public static void main(String[] args) throws Exception{
+        int n = 0;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            n = Integer.parseInt(br.readLine());
+        }
+        System.out.println(n);
+    }
+}
