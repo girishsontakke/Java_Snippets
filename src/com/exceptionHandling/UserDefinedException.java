4168691e28bd7d8cd74c4c88ad16389dfc9193fc
girishsontakke <girishsontakke7@gmail.com>
2020-10-29 10:07:54 +0530

    add default contructor

--- src/com/exceptionHandling/UserDefinedException.java
+++ src/com/exceptionHandling/UserDefinedException.java
@@ -7,6 +7,8 @@ public class UserDefinedException {
             int k = i/j;
             if(k==0){
                 throw new CustomException("this is custom exception");
+            }if(k<0){
+                throw new CustomException();
             }
 
         }catch (CustomException e){
@@ -16,7 +18,11 @@ public class UserDefinedException {
 }
 
 class CustomException extends Exception{
+    public CustomException(){
+        System.out.println("fix the error");
+    }
     public CustomException(String s) {
         super(s);
     }
+
 }
